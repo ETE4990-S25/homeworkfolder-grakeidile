e92f9fff3ff7b3be9daf6432ceee56d7c4bb182a
Grace Brisco <grakei.brisco@gmail.com>
2025-02-27 22:54:55 -0800

    cleaning up from my mega merge of all the files

--- Homework/data_classes.py
+++
@@ -1,6 +0,0 @@
-{
-    "Grace": "name",
-    "23": "age",
-    "gebrisco@cpp.edu": "email",
-    "123456789": "studentid"
-}
--- classNotes/Lecture 05/.ipynb_checkpoints/Hi-checkpoint.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-def print_hi():
-    print("hi")
-
-print("hellow world")
