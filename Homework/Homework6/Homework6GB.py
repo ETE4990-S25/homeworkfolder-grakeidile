b0c20c3814ebb55d7e3b5768d2498c423eca8520
Grace Brisco <grakei.brisco@gmail.com>
2025-03-11 17:11:51 -0700

    working on hw 6

---
+++ Homework/Homework6/Homework6GB.py
@@ -0,0 +1,15 @@
+#Basic Lambda Function
+#  Create a lambda function that takes one arguments and returns even or odd.
+#Advanced lambda Function
+#  Create a lambda function that takes a list and returns their sum 
+#Sorting with Lambda
+#Filtering with Lambda - `filter()` 
+#Mapping with Lambda - `map()`
+#Reducing with Lambda -  `reduce()` 
+#Enumerate with or without Lambda - `enumerate()`
+#zip with or without lambda (may combine enumerate like in class) - `zip()`
+
+even_odd = lambda x: 'The numer is even' if x % 2 == 0 else 'The number is odd'
+print (even_odd(5))
+
+sort = lambda y: list = {input} 
