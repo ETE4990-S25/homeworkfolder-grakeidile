d8036aea6bacc5521791a9155900571203a0132f
grakeidile <grakei.brisco@gmail.com>
2025-04-09 21:29:29 -0700

    in process

--- Project-2/project2GB.py
+++ Project-2/project2GB.py
@@ -1,6 +1,5 @@
 #Grace Brisco
-#sorry i didnt start sooner, Ive spent the entire break doing my senior project and working a lot
-#I know Im doing this last minute which is my fault, sorry again
+#sorry i didnt start sooner, Ive spent the entire break doing my senior project and working a lot to pay the bills
 
 
 import threading
@@ -16,41 +15,44 @@ def is_prime(n): #your function
             return False
     return True
 
-def highest_prime_threading(shared_prime): #Im not gonna lie, i tried to do this my own way without the shared, then the evil ai told me to used this shared prime and it worked (kinda)
-    start_time = time.time()               #Its only putting out a number sometimes so I will be back on this to fix it in the next day or so
-    number = 0
+def highest_prime_calculator(queue, duration = 180): #Im not gonna lie, i tried to do this my own way without the shared, then the evil ai told me to used a shared prime and then queue so i tried both 
+    start_time = time.time() #Its only putting out a number sometimes hrmmm
+    n = 0
+    highest_prime = 1
     while time.time() - start_time < 180:
-        if is_prime(number):
-            shared_prime.value = number
-        number += 1
+        if is_prime(n):
+            highest_prime = n #this is the kind of method I wanted to use on the game but wasnt sure how until now!
+        n += 1
+    queue.put(highest_prime)
+#i was checking a bunch of places like stackoverflow, quora and reddit, but they all used like different imports so this seemed like the best fit
 
-def highest_prime_mp(shared_prime): 
-    start_time = time.time()
-    number = 0
-    while time.time() - start_time < 180:
-        if is_prime(number):
-            shared_prime.value = number
-        number += 1
         
 def main():
-    highest_prime_threading_value = [0] #clearly the shared isnt working tho bc why the heck would they share a value when they are two different processes
+    duration = 180 #is this redundant to put it twice
 
-    thread = threading.Thread(target= highest_prime, args=(highest_prime_threading,))
-    thread.start()
-    thread.join()
-    print(f"The highest prime found: {highest_prime_threading_value[0]}")
+    print("finding your primes...i guess")
 
-    highest_prime_mp_value = multiprocessing.Value('i',0)
+    queue = multiprocessing.Queue()
+    mp_prime = multiprocessing.Process(target=highest_prime_calculator, args=(queue,duration))
+    mp_prime.start()
+    mp_prime.join()
 
-    prime_mp = multiprocessing.Process(target=highest_prime, args=(highest_prime_mp,))
-    prime_mp.start()
-    prime_mp.join()
-    print(f"The highest prime found is : {highest_prime_mp_value}")
+    t_prime = []
+    thread_prime = threading.Thread(target=)
+    thread_prime.start()
+    thread_prime.join()
 
+    a_prime = asyncio.run()
 
+    mp_prime = queue.get()
+    thread_prime = t_prime[0]
+    async_prime = a_prime
+
+    print(f"Multiprocessing : {mp_prime}")
+    print(f"Threading : {thread_prime}")
+    print(f"Async : {async_prime}")
 
 if __name__ == "__main__":
     main()
 #yes i know it doesnt run well at all
-#i have spent hours on this, I was working with the homies on it but I wanted to see if there were other ways to do it without totally ripping off of theirs like a freeloader
-#apologies again, it will be done
+#please spare me I am dying 
